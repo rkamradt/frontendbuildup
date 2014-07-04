67500e8601892d8e3fd74949931cb58824acf235
Randal Kamradt Sr <randysr@kamradtfamily.net>
2014-07-04 11:10:30 -0700

    make nyan's rainbow longer

--- client/view/mainview.js
+++ client/view/mainview.js
@@ -2,24 +2,6 @@ var _ = require('underscore');   // set up the underscore global handle.  _ is t
 var $ = require('jquery');
 var Backbone = require('backbone');
 
-/**
- *    <div class="container">
-
-      <form class="form-signin" role="form">
-        <h2 class="form-signin-heading">Please sign in</h2>
-        <input type="email" class="form-control" placeholder="Email address" required autofocus>
-        <input type="password" class="form-control" placeholder="Password" required>
-        <label class="checkbox">
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      </form>
-
-    </div> <!-- /container -->
-*/
-
-
-
 module.exports = Backbone.View.extend({
   el:'body',
   render: function() {
@@ -27,19 +9,11 @@ module.exports = Backbone.View.extend({
       append($("<div />", { "class": "container" }).
         append($("<form />", { "class": "form-signin", "role": "form"}).
           append($("<h2 />", {"class": "form-signin-heading"}).html("Please sign in")).
-          append($("<input />", { "type": "email", "class": "form-control", "placeholder": "Email address", "required": true, "autofocus": true })).
-          append($("<input />", { "type": "password", "class": "form-control", "placeholder": "Password", "required": true })).
+          append($("<input />", { "name": "email", "type": "email", "class": "form-control", "placeholder": "Email address", "required": true, "autofocus": true })).
+          append($("<input />", { "name": "password", "type": "password", "class": "form-control", "placeholder": "Password", "required": true })).
           append($("<label />", { "class": "checkbox" }).
-            append($("<input />", { "type": "checkbox", "value": "remember-me"})).
+            append($("<input />", { "name": "rememberme", "type": "checkbox", "value": "remember-me"})).
             append($("<span />").html("Remember me"))).
           append($("<button />", { "class": "btn btn-lg btn-primary btn-block", "type": "submit"}).html("Sign in"))));
-      /*
-        <input type="email" class="form-control" placeholder="Email address" required autofocus>
-        <input type="password" class="form-control" placeholder="Password" required>
-        <label class="checkbox">
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-        */
   } 
 });
--- test/main.test.js
+++ test/main.test.js
@@ -1,5 +1,5 @@
 var Browser = require("zombie");
-require('should');
+var should = require('should');
 
 describe('Main', function(){
   describe('#index', function(){
@@ -10,8 +10,27 @@ describe('Main', function(){
         .visit("http://localhost:9999/index.html")
         .then(done, done);
     });
-    it('should display "Hello World" in the browser', function(){
-      this.browser.html("body").should.equal("<body>Hello World</body>");
+    it('should have an email input field in the browser that is required', function(){
+      var email = this.browser.html("input[name=email]");
+      should.exist(email);
+      email.should.containEql('required')
+      email.should.containEql('type="email"')
+    });
+    it('should have an password input field in the browser that is required', function(){
+      var password = this.browser.html("input[name=password]");
+      should.exist(password);
+      password.should.containEql('required');
+      password.should.containEql('type="password"');
+    });
+    it('should have an checkbox in the browser that will retain logon info (tested elsewhere)', function(){
+      var checkbox = this.browser.html("input[name=rememberme]");
+      should.exist(checkbox);
+      checkbox.should.containEql('type="checkbox"')
+    });
+    it('should have a submit button that will submit the form (tested elsewhere)', function(){
+      var button = this.browser.html("button");
+      should.exist(button);
+      button.should.containEql('type="submit"')
     });
   });
 });
